06b7a6b85aa47e3f55269c4d4dbb87f923a65dfd
Jinamizi <tonnylango@gmail.com>
2023-06-12 11:29:09 +0300

    updated

--- isValidSubsequence.py
+++ isValidSubsequence.py
@@ -4,13 +4,14 @@ def check(array, sequence):
         for i in range(0, len(sequence)):
             print(f"sequence[{i}]:", sequence[i], " position:", position)
             
-            position += array[position:].index(sequence[i])
-            print(f"position of sequnce[{i}] in array {position}")
-            position += 1
+            #position += array[position:].index(sequence[i])
+            position += array.index(sequence[i], position)
+            position += 1 #so as to start searching array from the next element after the current
         return True
     except Exception:
         return False
 
+#this mutates the sequence array, which may result in unexpected behavior, ensure array is copied.
 def check2(array, sequence):
     # Loop through the array in reverse order
     for i in range(len(array) - 1, -1, -1):
@@ -19,12 +20,13 @@ def check2(array, sequence):
         if array[i] == sequence[-1]:  # Check if current element matches the last element in the sequence
             sequence.pop()  # Remove the last element from the sequence
 
-    return return not sequence
- # Return True if the sequence is empty, otherwise False
+    # Return True if the sequence is empty, otherwise False
+    return not sequence
+ 
 
 
 if __name__ == "__main__":
     array = [1,1,1,6]
     sequence = [1,1,1,1]
 
-    print(check2(array, sequence))
+    print(check(array, sequence))
